b7794684c980c7b103a03051f1ffbeed557925dc
Deeya Dave <deeyadave06@gmail.com>
2025-11-19 10:45:37 -0500

    "Unit 12 Exercise"

--- inventory/footer.inc.php
+++ inventory/footer.inc.php
@@ -1,6 +1,5 @@
 <p>&copy Inventory Helper - Making Inventory Management Easier</p>
-<p>Name, Course & Section, Course Name, Assignment, yourid@njit.edu</p>
-<p>
+<p>Deeya Dave, IT-202-003, Internet Applications, Unit 12 In-Class Exercise, dd594@njit.edu, 11/19/2025</p>
    <?php
    date_default_timezone_set("America/New_York");
    echo "The date and time is " . date("D M j h:ia T Y");
